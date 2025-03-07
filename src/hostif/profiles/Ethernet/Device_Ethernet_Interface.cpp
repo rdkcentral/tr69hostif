70d2b122b45ae26d4fc788743751f71cfdbba1df
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-03-07 13:44:54 -0500

    Update Device_Ethernet_Interface.cpp

--- src/hostif/profiles/Ethernet/Device_Ethernet_Interface.cpp
+++ src/hostif/profiles/Ethernet/Device_Ethernet_Interface.cpp
@@ -786,11 +786,11 @@ int hostIf_EthernetInterface::set_Device_Ethernet_Interface_Enable(HOSTIF_MsgDat
 
     if(FALSE == value)
     {
-        rc = system("ifconfig eth%d down", ethInterfNo-1);
+        rc = v_secure_system("background ifconfig eth%d down", ethInterfNo-1);
     }
     else
     {
-        rc = system("ifconfig eth%d up", ethInterfNo-1);
+        rc = v_secure_system("background ifconfig eth%d up", ethInterfNo-1);
     }
 
     if(rc < 0)
