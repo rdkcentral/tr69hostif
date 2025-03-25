29163ae1ea323c1b689bd03e0f6dd981b5a411db
Vismalskumar0 <ajvismal@yahoo.com>
2025-03-25 15:07:31 +0530

    Update hostIf_main.h

--- src/hostif/include/hostIf_main.h
+++ src/hostif/include/hostIf_main.h
@@ -110,8 +110,15 @@
 
 extern gchar *date_str;
 
+typedef enum {
+    MERGE_SUCCESS,
+    MERGE_FAILURE
+} MergeStatus;
+
+
+
 void tr69hostIf_logger (const gchar *log_domain, GLogLevelFlags log_level,const gchar *message, gpointer user_data);
- void mergeDataModel();
+MergeStatus mergeDataModel();
 void filter_and_merge_xml(const char *input1, const char *input2, const char *output);
 
 #define G_LOG_DOMAIN    ((gchar*) 0)
