7c7ecf5e49917978afe87060f082372df7943530
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-04-11 16:03:06 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -32,7 +32,7 @@ rm -rf remote_debugger
 git clone https://github.com/rdkcentral/rdk-halif-device_settings.git
 git clone https://github.com/rdkcentral/rdkvhal-devicesettings-raspberrypi4.git
 git clone https://github.com/rdkcentral/iarmbus.git
-git clone https://github.com/rdkcentral/remote_debugger.git
+git clone https://github.com/rdkcentral/remote_debugger.git -b feature/rrdl2
 
 # Build devicesettings version with fixes for native build and use that as a stub
 # TODO This is not present in mainline versions. Component maintainers will have to provide this in future.
