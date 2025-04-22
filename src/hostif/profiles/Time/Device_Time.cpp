d5d4976f4cd04791aa1b10e32390b8a8dbe557da
Vismal S Kumar <ajvismal@yahoo.com>
2025-04-22 13:37:59 +0530

    Update Device_Time.cpp

--- src/hostif/profiles/Time/Device_Time.cpp
+++ src/hostif/profiles/Time/Device_Time.cpp
@@ -187,13 +187,13 @@ int hostIf_Time::get_Device_Time_LocalTimeZone(HOSTIF_MsgData_t *stMsgData, bool
     }
 
     bCalledLocalTimeZone = true;
-    rc=strcpy_s(stMsgData->paramValue,_BUF_LEN_64-1,tmp)
+    rc=strcpy_s(stMsgData->paramValue, _BUF_LEN_64, tmp)
     if(rc!=EOK)
     {
         ERR_CHK(rc);
     }
     
-    rc=strcpy_s(backupLocalTimeZone,_BUF_LEN_64-1,tmp);
+    rc=strcpy_s(backupLocalTimeZone, _BUF_LEN_64, tmp);
     if(rc!=EOK)
     {
         ERR_CHK(rc);
