bcd3fdfe528165f095a8f3dde81734d9ddc93ef2
Vismal S Kumar <ajvismal@yahoo.com>
2025-04-23 16:02:43 +0530

    Update startParodus.cpp

--- src/hostif/parodusClient/startParodus/startParodus.cpp
+++ src/hostif/parodusClient/startParodus/startParodus.cpp
@@ -81,7 +81,8 @@ std::string get_HWMAcAddress()
     fp = fopen(HWMAC_FILE, "r");
     if (fp != NULL)
     {
-        size_t bytesRead = fread(tempMAC, 1, 17, fp);
+        //size_t bytesRead = fread(tempMAC, 1, 17, fp);
+        fread(tempMAC, 1, 17, fp);
        // RDK_LOG(RDK_LOG_DEBUG, LOG_TR69HOSTIF, "Bytes read: %zu\n", bytesRead);
         for (srcCount = 0; dstCount < 12 && srcCount < 17; srcCount++)
         {
