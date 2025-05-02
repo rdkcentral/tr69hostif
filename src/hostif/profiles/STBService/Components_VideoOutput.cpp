1c1f0bdef8ffbd377f98471da9b9a8ad9a8fa92a
Vismal S Kumar <ajvismal@yahoo.com>
2025-05-02 13:15:30 +0530

    Update Components_VideoOutput.cpp

--- src/hostif/profiles/STBService/Components_VideoOutput.cpp
+++ src/hostif/profiles/STBService/Components_VideoOutput.cpp
@@ -138,7 +138,7 @@ void hostIf_STBServiceVideoOutput::getLock()
 
 void hostIf_STBServiceVideoOutput::releaseLock()
 {
-    RDK_LOG(RDK_LOG_INFO,LOG_TR69HOSTIF,"[%s:%d] Unlocking mutex...  \n", __FUNCTION__, __LINE__);
+    RDK_LOG(RDK_LOG_DEBUG,LOG_TR69HOSTIF,"[%s:%d] Unlocking mutex...  \n", __FUNCTION__, __LINE__);
     g_mutex_unlock(&hostIf_STBServiceVideoOutput::m_mutex);
 }
 
