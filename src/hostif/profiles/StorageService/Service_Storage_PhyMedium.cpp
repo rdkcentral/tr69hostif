96360645ac2096a8f05e9033daa5481d03d5bd14
Vismal S Kumar <ajvismal@yahoo.com>
2025-05-02 13:08:59 +0530

    Update Service_Storage_PhyMedium.cpp

--- src/hostif/profiles/StorageService/Service_Storage_PhyMedium.cpp
+++ src/hostif/profiles/StorageService/Service_Storage_PhyMedium.cpp
@@ -109,7 +109,7 @@ void hostIf_PhysicalMedium::getLock()
  */
 void hostIf_PhysicalMedium::releaseLock()
 {
-    RDK_LOG(RDK_LOG_INFO,LOG_TR69HOSTIF,"[%s:%d] Unlocking mutex...  \n", __FUNCTION__, __LINE__);
+    RDK_LOG(RDK_LOG_DEBUG,LOG_TR69HOSTIF,"[%s:%d] Unlocking mutex...  \n", __FUNCTION__, __LINE__);
     g_mutex_unlock(m_mutex);
 }
 
