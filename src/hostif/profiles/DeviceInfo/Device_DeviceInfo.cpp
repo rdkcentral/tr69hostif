d0afb4dd5f31f0594e4ea8206792434c92f65d0a
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2025-07-03 23:16:46 +0530

    Update Device_DeviceInfo.cpp

--- src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
+++ src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
@@ -3905,7 +3905,7 @@ int hostIf_DeviceInfo::set_xRDKCentralComRFC(HOSTIF_MsgData_t * stMsgData)
     }
     else if (strcasecmp(stMsgData->paramName,RDK_REMOTE_DEBUGGER_GETPROFILE_DATA) == 0)
     {
-        ret = set_Device_DeviceInfo_X_RDKCENTRAL_COM_RDKRemoteDebuggergetProfileData(stMsgData);
+        ret = get_Device_DeviceInfo_X_RDKCENTRAL_COM_RDKRemoteDebuggergetProfileData(stMsgData);
     }
 	    
 #endif
