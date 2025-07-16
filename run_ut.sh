13852f0fa4a16e5ee7a4fc550ac0f4cc9c7e925c
nhanasi <navihansi@gmail.com>
2025-07-16 14:19:45 -0400

    Update run_ut.sh

--- run_ut.sh
+++ run_ut.sh
@@ -17,6 +17,17 @@
  #
  # SPDX-License-Identifier: Apache-2.0
  ############################################################################
+ENABLE_COV=false
+
+if [ "x$1" = "x--enable-cov" ]; then
+      echo "Enabling coverage options"
+      export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+      export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+      export LDFLAGS="-lgcov --coverage"
+      ENABLE_COV=true
+fi
+
+export TOP_DIR=`pwd`
 
 apt-get update
 apt-get -y install libtinyxml2-dev
@@ -58,10 +69,15 @@ make
 echo "********************"
 
 
-lcov --capture --directory . --output-file coverage.info
-lcov --remove coverage.info '/usr/*' --output-file coverage.filtered.info
-genhtml coverage.filtered.info --output-directory out
+if [ "$ENABLE_COV" = true ]; then
+    echo "Generating coverage report"
+    lcov --capture --directory . --output-file coverage.info
+    lcov --remove coverage.info '/usr/*' --output-file coverage.info
+    lcov --list coverage.info
+    genhtml coverage.filtered.info --output-directory out
+fi
 
+cd $TOP_DIR
 
 echo "**** Compiling DeviceInfo gtest ****"
 cd $TOP_DIR/src/hostif/profiles/DeviceInfo/gtest
@@ -69,8 +85,3 @@ rm devieInfo_gtest
 make
 ./devieInfo_gtest
 echo "********************"
-
-
-lcov --capture --directory . --output-file coverage.info
-lcov --remove coverage.info '/usr/*' --output-file coverage.filtered.info
-genhtml coverage.filtered.info --output-directory out
