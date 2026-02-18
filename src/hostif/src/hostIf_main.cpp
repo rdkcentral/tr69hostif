ff81cf4860ffa9d8b48b5ecfecdae474e0b34d65
Vismal S Kumar <ajvismal@yahoo.com>
2026-02-19 03:35:08 +0530

    DELIA-69998: tr69hostif crash seen in device boot-up during CDL migration/Reboot test (#365)
    * DELIA-69998-[Stress][Sercomm RTK ES1]: tr69hostif crash seen in device boot-up during CDL migration/Reboot test
    
    DELIA-69998-[Stress][Sercomm RTK ES1]: tr69hostif crash seen in device boot-up during CDL migration/Reboot test
    
    * Update hostIf_updateHandler.cpp
    
    * Update hostIf_main.cpp

--- src/hostif/src/hostIf_main.cpp
+++ src/hostif/src/hostIf_main.cpp
@@ -614,8 +614,9 @@ void exit_gracefully (int sig_received)
 
             /*Stop libSoup server and exit Json Thread */
             hostIf_HttpServerStop();
-
+            // Stop update polling and wait for the worker to exit before further teardown
             updateHandler::stop();
+			updateHandler::join();
             XBSStore::getInstance()->stop();
 
             if(logfile) fclose (logfile);
