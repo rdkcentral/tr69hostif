3853b2bf40f0feaa20ba6f3d86b524eba5e5f269
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-03-05 16:08:28 +0530

    Update Device_Time.h

--- src/hostif/profiles/Time/Device_Time.h
+++ src/hostif/profiles/Time/Device_Time.h
@@ -284,7 +284,15 @@ public:
     int get_Device_Time_NTPMaxpoll(HOSTIF_MsgData_t *,bool *pChanged = NULL);
     int set_Device_Time_NTPMaxpoll(HOSTIF_MsgData_t *,bool *pChanged = NULL);
     int get_Device_Time_NTPServer1Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
-   int set_Device_Time_NTPServer1Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int set_Device_Time_NTPServer1Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int get_Device_Time_NTPServer2Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int set_Device_Time_NTPServer2Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int get_Device_Time_NTPServer3Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int set_Device_Time_NTPServer3Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int get_Device_Time_NTPServer4Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int set_Device_Time_NTPServer4Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int get_Device_Time_NTPServer5Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
+    int set_Device_Time_NTPServer5Directive(HOSTIF_MsgData_t*, bool* pChanged = NULL);
 
     /**
      * @brief    Get the local time zone definition.
