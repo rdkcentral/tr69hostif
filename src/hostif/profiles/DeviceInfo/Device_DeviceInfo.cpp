23c03e3a13e675bb1f64a9a8e45b2dc004537d5c
rwarier <84991591+rwarier@users.noreply.github.com>
2026-07-29 11:02:39 -0500

    RDKEMW-21798: Test change, do not merge

--- src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
+++ src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
@@ -742,7 +742,7 @@ int hostIf_DeviceInfo::get_Device_DeviceInfo_ModelName(HOSTIF_MsgData_t * stMsgD
 {
     int ret=NOT_HANDLED;
     stMsgData->paramtype = hostIf_StringType;
-#if 0 #!defined (USE_DEV_PROPERTIES_CONF)
+#if 0 // !defined (USE_DEV_PROPERTIES_CONF)
     IARM_Bus_MFRLib_GetSerializedData_Param_t param;
     IARM_Result_t iarm_ret = IARM_RESULT_IPCCORE_FAIL;
     memset(&param, 0, sizeof(param));
