42746ce7c149988caf9027eb830a5910b87f21d0
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-08-08 18:46:37 +0530

    Update cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -78,6 +78,12 @@ make \
     install
     
 cd ${ROOT}
+rm -rf iarmmgrs
+git clone https://github.com/rdkcentral/iarmmgrs.git
+cp iarmmgrs/sysmgr/include/sysMgr.h /usr/local/include
+cp iarmmgrs/maintenance/include/maintenanceMGR.h /usr/local/include
+
+cd ${ROOT}
 rm -rf telemetry
 git clone https://github.com/rdkcentral/telemetry.git
 cd telemetry
