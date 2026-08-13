4d46c331d043b5f66e8f3eee607fb148a9e33c4c
Konkati_Bhavani <Konkati_Bhavani@comcast.com>
2026-08-13 10:32:18 +0000

    Adding the required headers

--- src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
+++ src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
@@ -40,6 +40,10 @@
 
 #include <cmath>
 #include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/sysinfo.h>
