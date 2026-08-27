6b0beef3d6d38201d7aceb56bd5731b0ca27bb58
rdkdevpilot <svc-rdk-devpilot@cable.comcast.com>
2026-08-27 15:07:21 +0530

    Fix NO_EFFECT in Components_HDMI.cpp
    Defect ID: c95ce562-2450-4888-bd37-cbf41ce6d045

--- src/hostif/profiles/STBService/Components_HDMI.cpp
+++ src/hostif/profiles/STBService/Components_HDMI.cpp
@@ -493,21 +493,18 @@ int hostIf_STBServiceHDMI::setResolution(const HOSTIF_MsgData_t *stMsgData)
 int hostIf_STBServiceHDMI::getResolutionValue(HOSTIF_MsgData_t *stMsgData,bool *pChanged)
 {
     try {
-        if(vPort.getResolution().getPixelResolution().getName().c_str() && vPort.getResolution().getFrameRate().getName().c_str())
-        {
-            snprintf(stMsgData->paramValue, PARAM_LEN, "%s%s/%sHz",
-                    vPort.getResolution().getPixelResolution().getName().c_str(),
-                    vPort.getResolution().isInterlaced()?"i":"p",
-                    vPort.getResolution().getFrameRate().getName().c_str());
+        snprintf(stMsgData->paramValue, PARAM_LEN, "%s%s/%sHz",
+                vPort.getResolution().getPixelResolution().getName().c_str(),
+                vPort.getResolution().isInterlaced()?"i":"p",
+                vPort.getResolution().getFrameRate().getName().c_str());
 
-            if(bCalledResolutionValue && pChanged && strcmp(backupResolutionValue, stMsgData->paramValue))
-            {
-                *pChanged = true;
-            }
-            bCalledResolutionValue = true;
-            strncpy(backupResolutionValue, stMsgData->paramValue, _BUF_LEN_16-1);
-            backupResolutionValue[_BUF_LEN_16-1] = '\0';
+        if(bCalledResolutionValue && pChanged && strcmp(backupResolutionValue, stMsgData->paramValue))
+        {
+            *pChanged = true;
         }
+        bCalledResolutionValue = true;
+        strncpy(backupResolutionValue, stMsgData->paramValue, _BUF_LEN_16-1);
+        backupResolutionValue[_BUF_LEN_16-1] = '\0';
         stMsgData->paramtype = hostIf_StringType;
         stMsgData->paramLen = strlen(stMsgData->paramValue);
         RDK_LOG(RDK_LOG_DEBUG,LOG_TR69HOSTIF,"[%s] getHDMIResolution(): Value: %s\n",__FUNCTION__, stMsgData->paramValue);
