267fff5a789b22e54cabcf135c5dbb5978456562
Abhinavpv28 <162570454+Abhinavpv28@users.noreply.github.com>
2026-09-16 08:46:24 +0530

    Update Device_DeviceInfo.cpp

--- src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
+++ src/hostif/profiles/DeviceInfo/Device_DeviceInfo.cpp
@@ -2706,9 +2706,10 @@ int hostIf_DeviceInfo::set_xOpsDMUploadLogsNow (HOSTIF_MsgData_t *stMsgData)
 
     if(triggerUploadLog)
     {
-		RDK_LOG(RDK_LOG_DEBUG,LOG_TR69HOSTIF,"[%s] Start executing script to upload logs API... \n",__FUNCTION__);
-        triggerUploadLogsNow();
-        RDK_LOG(RDK_LOG_INFO,LOG_TR69HOSTIF,"Successfully executed Logupload API \n");
+        RDK_LOG(RDK_LOG_DEBUG,LOG_TR69HOSTIF,"[%s] Queueing log upload API... \n",__FUNCTION__);
+        std::thread uploadThread(triggerUploadLogsNow);
+        uploadThread.detach();
+        RDK_LOG(RDK_LOG_INFO,LOG_TR69HOSTIF,"Log upload API queued successfully\n");
     }
     else
     {
